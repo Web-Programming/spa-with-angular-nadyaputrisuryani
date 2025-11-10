44a811c7284c1a82f9a0aaf661bcd30b874d4f1a
nadyaputrisuryani <nputrisuryani@gmail.com>
2025-11-10 16:55:28 +0700

    10/11

---
+++ griya-mdp/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import {Home as HomeComponent } from './home/home';
+export const routes: Routes = [
+    {
+        path :"",
+        component : HomeComponent,
+        title :'Home Page',
+    }
+];
