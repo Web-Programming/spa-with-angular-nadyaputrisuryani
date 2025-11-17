759bc41ae8ba686b9f9f8d825eba01d6e3a3d204
nadyaputrisuryani <nputrisuryani@gmail.com>
2025-11-17 16:02:52 +0700

    latihan

--- griya-mdp/src/app/app.routes.ts
+++ griya-mdp/src/app/app.routes.ts
@@ -1,9 +1,40 @@
 import { Routes } from '@angular/router';
 import {Home as HomeComponent } from './home/home';
+import { Profile } from './profile/profile';
+import { Login } from './login/login';
+import { Contact } from './contact/contact';
+import { LokasiPerumahan } from './lokasi-perumahan/lokasi-perumahan';
 export const routes: Routes = [
     {
         path :"",
         component : HomeComponent,
-        title :'Home Page',
-    }
+        title:'Home Page'
+    },
+    {
+        path :"profile",
+        component : Profile,
+        //title:'Profile'
+        
+    },
+        {
+        path :"login",
+        component : Login,
+        //title:'Login'
+        
+    },
+        {
+        path :"contact",
+        component : Contact,
+        //title:'Contact'
+        
+    },
+     {
+        path :"lokasi",
+        component : LokasiPerumahan,
+        //title:'Lokasi '
+        
+    },
+
+
+
 ];
